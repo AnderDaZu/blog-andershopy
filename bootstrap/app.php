0bc1a361f84fec514e2131b480ede68c1803a25a
anderDaZu <anderda-96@hotmail.com>
2024-06-02 00:21:17 -0500

    Creando y configurando otro archivo de rutas para laravel 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,13 +3,35 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        // Si se require cambiar alguna configuración de alguno de los archivos de ruta preestablecidos, se agrega lo siguiente en using, 
+        // y se debe comentar tanto web como api
+        // using: function () {
+        //     Route::middleware('api')
+        //          ->prefix('api')
+        //          ->group(base_path('routes/api.php'));
+
+        //     Route::middleware('web')
+        //          ->group(base_path('routes/web.php'));
+
+        //     Route::middleware('web')
+        //          ->prefix('admin')
+        //          ->group(base_path('routes/admin.php'));
+
+        // },
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        // then permite registrar nuevos archivos de ruta
+        then: function () {
+            Route::middleware('web')
+                ->prefix('admin')
+                ->group(base_path('routes/admin.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //

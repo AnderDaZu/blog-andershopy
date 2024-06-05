0151ee4d4d130bb71042c2228e24c986fa2f8aec
anderDaZu <anderda-96@hotmail.com>
2024-06-05 10:20:43 -0500

    Rutas para crud de posts

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('admin.posts.index');
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function edit(Post $post)
+    {
+        return view('admin.posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    public function destroy(Post $post)
+    {
+        //
+    }
+}

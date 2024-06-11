ccac38d99b3d66fdad3abb8796b68df94c15c55d
anderDaZu <anderda-96@hotmail.com>
2024-06-10 21:17:17 -0500

    Métodos para obtener archivos y directorios

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,3 +21,15 @@ Route::get('/tags', function (Request $request) {
             ];
         });
 })->name('api.tags.index');
+
+// Archivos y directorios
+Route::get('/files', function (Request $request) {
+    // método para obtener todos los archivos del directorio especificado
+    // Storage::files('posts'); // public/storage/posts
+    // método para obtener todos los archivos del directorio y subdirectorios del directorio especificado 
+    // Storage::allFiles('posts'); // public/storage/posts
+    // metodo para obtener todos los directorios del directorio especificado
+    // Storage::directories('posts');
+    // metodo para obtener todos los directorios del directorio y subdirectorios del directorio especificado
+    // Storage::allDirectories('posts');
+})->name('api.files.index');

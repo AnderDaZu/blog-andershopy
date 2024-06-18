96d33a1d379d622f7dce59fb7ef0132748fc1ed9
anderDaZu <anderda-96@hotmail.com>
2024-06-17 21:16:39 -0500

    Agregando Policy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Post;
 use App\Models\User;
 use App\Observers\PostObserver;
+use App\Policies\PostPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -29,9 +30,10 @@ class AppServiceProvider extends ServiceProvider
             return $user->is_admin;
         });
 
+        // En vez de serguir agregando gates que incluyan más de 1 objeto 👇, mejor se pasa a un policy todas las validaciones de acceso a crear
         // al momento de llamar este gate, espera que como parametro se le pase un post, el user ya lo capta por defecto
-        Gate::define('author', function ($user, $post) {
-            return $user->id === $post->user_id;
-        });
+        // Gate::define('author', function ($user, $post) {
+        //     return $user->id === $post->user_id;
+        // });
     }
 }
